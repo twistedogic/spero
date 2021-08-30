3abcdc145a417a8b33aa340215c2b654064a5454
Jordan Li <twistedogic@gmail.com>
2021-08-30 23:29:14 +0800

    refactor(client): set default user-agent for request

--- pkg/client/base/base.go
+++ pkg/client/base/base.go
@@ -5,6 +5,10 @@ import (
 	"net/http"
 )
 
+const (
+	userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36"
+)
+
 type Client struct {
 	*http.Client
 }
@@ -15,7 +19,12 @@ func New() Client {
 }
 
 func (c Client) GetByte(u string) ([]byte, error) {
-	res, err := c.Get(u)
+	req, err := http.NewRequest("GET", u, nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("User-Agent", userAgent)
+	res, err := c.Do(req)
 	if err != nil {
 		return nil, err
 	}
