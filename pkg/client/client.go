2b1cc43418010f3c7d84530db2a874d25479e7d4
Jordan Li <twistedogic@gmail.com>
2021-09-05 02:59:00 +0800

    refactor(client): use default http.Client

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -1,6 +1,7 @@
-package base
+package client
 
 import (
+	"context"
 	"io/ioutil"
 	"net/http"
 )
@@ -9,22 +10,15 @@ const (
 	userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36"
 )
 
-type Client struct {
-	*http.Client
-}
+var client = new(http.Client)
 
-func New() Client {
-	client := &http.Client{}
-	return Client{Client: client}
-}
-
-func (c Client) GetByte(u string) ([]byte, error) {
-	req, err := http.NewRequest("GET", u, nil)
+func GetByte(ctx context.Context, u string) ([]byte, error) {
+	req, err := http.NewRequestWithContext(ctx, "GET", u, nil)
 	if err != nil {
 		return nil, err
 	}
 	req.Header.Set("User-Agent", userAgent)
-	res, err := c.Do(req)
+	res, err := client.Do(req)
 	if err != nil {
 		return nil, err
 	}
--- pkg/client/oddclient/oddclient.go
+++ pkg/client/oddclient/oddclient.go
@@ -1,31 +1,63 @@
 package oddclient
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
 	"strings"
 
-	"github.com/twistedogic/spero/pkg/client/base"
+	"github.com/Jeffail/benthos/v3/public/service"
+
+	"github.com/twistedogic/spero/pkg/client"
+	"github.com/twistedogic/spero/pkg/message"
+	"github.com/twistedogic/spero/pkg/poll"
 )
 
 const (
 	DefaultURL = "https://bet.hkjc.com/football/getJSON.aspx"
 )
 
+type data struct {
+	Matches []json.RawMessage `json:"matches"`
+}
+
+func flattenOddResponse(b []byte) ([]*service.Message, error) {
+	items := make([]data, 0)
+	if err := json.Unmarshal(b, &items); err != nil {
+		return nil, err
+	}
+	msgs := make([]*service.Message, 0)
+	for _, item := range items {
+		for _, match := range item.Matches {
+			msgs = append(msgs, message.NewContentHashMessage(match))
+		}
+	}
+	return msgs, nil
+}
+
 type Client struct {
-	base.Client
 	BaseURL string
 }
 
 func New(oddURL string) Client {
-	client := base.New()
-	return Client{client, oddURL}
+	return Client{BaseURL: oddURL}
 }
 
 func NewWithDefault() Client {
 	return New(DefaultURL)
 }
 
-func (c *Client) GetOddTable(bettype string) ([]byte, error) {
+func (c Client) getOddTable(ctx context.Context, bettype string) ([]byte, error) {
 	u := fmt.Sprintf("%s?jsontype=odds_%s.aspx", c.BaseURL, strings.ToLower(bettype))
-	return c.GetByte(u)
+	return client.GetByte(ctx, u)
+}
+
+func (c Client) PollOdd(bettype string) poll.PollFunc {
+	return func(ctx context.Context) ([]*service.Message, error) {
+		b, err := c.getOddTable(ctx, bettype)
+		if err != nil {
+			return nil, err
+		}
+		return flattenOddResponse(b)
+	}
 }
